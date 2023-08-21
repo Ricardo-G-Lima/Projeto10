636abe790caace080711e954de3512e9f105eba3
3Amodular <3mod@bglab.local>
2023-08-21 17:28:19 -0300

    adicionando restrições no modelLogin

--- PROJETO10/Models/modelLogin.cs
+++ PROJETO10/Models/modelLogin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,8 +13,15 @@ namespace PROJETO10.Models
         [Key]
         public int CODIGO { get; set; }
 
+        [DisplayName("Usúario")]
+        [StringLength(50, ErrorMessage = "O nome deve conter no máx 50 caracteres")]
+        [Required(ErrorMessage = " O campo Usúario é obrigatório")]
         public string USUARIO { get; set; }
 
+        [DisplayName("Senha")]
+        [StringLength(50, MinimumLength = 6, ErrorMessage ="A senha deve conter no máx 50 caracteres e no min 6")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage =" O campo Senha é obrigatório")]
         public string SENHA { get; set; }
 
     }
